e09d8d3a1a71537ec3575d803843c59b67a92c17
Maksim Perov <coder@frtk.ru>
2021-04-13 18:53:41 +0300

    clone_and_patch is improved

--- clone_and_patch.sh
+++ clone_and_patch.sh
@@ -11,3 +11,11 @@ mkdir build
 cd build
 ../configure --target-list=aarch64-linux-user
 make
+
+echo -e ""
+echo -e "Now, you may run aarch64 binary on QEMU like here:"
+echo -e "./qemu/build/qemu-aarch64 -L /usr/aarch64-linux-gnu <name-of-your-binary>"
+echo -e "and you may see similar to it:"
+echo -e "current pc: 55018d1e30\ncurrent pc: 55018d1eb8\ncurrent pc: 55018d1eb8\ncurrent pc: 55018d1eb8"
+echo -e "current pc: 55018d1eb8\ncurrent pc: 55018d1ecc\ncurrent pc: 55018d1e78\ncurrent pc: 55018d1e78"
+echo -e ""
